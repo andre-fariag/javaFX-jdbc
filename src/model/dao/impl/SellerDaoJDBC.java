e88e45d066b630865653d02c374d666c2f68ba6f
André Faria <andrefaria-1@hotmail.com>
2020-06-28 17:31:13 -0300

    TextField & DatePicker

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -150,7 +150,7 @@ public class SellerDaoJDBC implements SellerDao {
 		obj.setId(rs.getInt("Id"));
 		obj.setName(rs.getString("Name"));
 		obj.setEmail(rs.getString("Email"));
-		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setBirthDate(new java.util.Date(rs.getTimestamp("BirthDate").getTime()));
 		obj.setBaseSalaray(rs.getDouble("BaseSalary"));
 		obj.setDepartment(dep);
 		return obj;
